5e2e9b87a7a82ca3db2d4753ddb0f721661f15b9
VXshao <36029605+VXshao@users.noreply.github.com>
2018-02-12 22:50:40 +0800

    Add files via upload

---
+++ FanQieZhong/FanQieZhong/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace FanQieZhong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Image = global::FanQieZhong.Properties.Resources._006xvkvkgy1fnibigklprj30ja0vzwid;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(2, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始计时";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Image = global::FanQieZhong.Properties.Resources._006xvkvkgy1fnibigklprj30ja0vzwid;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.Location = new System.Drawing.Point(232, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "放弃本次";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.progressBar1.Location = new System.Drawing.Point(2, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(339, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "设定一个时间";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(81, 25);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(131, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "累计番茄数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(160, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(248, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "by-VeinsVX";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Image = global::FanQieZhong.Properties.Resources._006xvkvkgy1fnibigklprj30ja0vzwid;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Location = new System.Drawing.Point(117, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "暂停计时";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label5.Location = new System.Drawing.Point(18, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "00h00m00s";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox1.Location = new System.Drawing.Point(232, 81);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(136, 24);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "保留数据到本地";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(99, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "min";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BackgroundImage = global::FanQieZhong.Properties.Resources._006xvkvkgy1fnibigklprj30ja0vzwid;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(343, 132);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "番茄时钟";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ FanQieZhong/FanQieZhong/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace FanQieZhong
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "SetForegroundWindow")]
+        //设置窗体句柄的窗体为活动窗体，因为到后面如果此程序并不在焦点中，可以是此程序回归活动窗体
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private int Timer;//用户设置的倒计时的时间
+        private DateTime startTime;//用来保存开始番茄钟的时间
+        private bool flag=false;//用来控制是否保存数据到本地
+        int hours;//用来保存转换后的小时数
+        int min;//保存分钟数
+        int second;//保存秒数
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = 0.ToString();
+            checkBox1.Checked = true;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            flag = checkBox1.Checked;//开始时获取用户是否要保存文件到本地
+            startTime = DateTime.Now.ToLocalTime();//记录此次番茄钟开始时间，用来后期数据保存
+            Timer = Convert.ToInt32(textBox1.Text)*60;
+            button2.Text = "暂停计时";
+            timer1.Enabled = true;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = Timer;
+            progressBar1.Step = 1;
+        }
+        /// <summary>
+        /// 用计时器来控制实现 时间的减少
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if(Timer>0)
+            {
+                Timer -= 1;
+                hours=Timer / 3600;
+                min = (Timer - 3600 * hours) / 60;
+                second = Timer % 60;
+                label5.Text = hours.ToString() + "h" + min.ToString() + "m" + second.ToString() + "s";
+                progressBar1.Value = Timer;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                SetForegroundWindow(this.Handle);
+                MessageBox.Show("时间到了，休息一会吧^_^");
+                SaveDate();
+            }
+        }
+        /// <summary>
+        /// 保存数据
+        /// </summary>
+        private void SaveDate()
+        {
+            int i = Convert.ToInt32(label3.Text);
+            i += 1;
+            label3.Text = i.ToString();
+            if(flag)
+            {
+                string directoryPath = @"C:\TomatoDate";//定义一个路径变量
+                string filePath = "TomatoDate.txt";//定义一个文件路径变量
+                if (!Directory.Exists(directoryPath))//如果路径不存在
+                {
+                    Directory.CreateDirectory(directoryPath);//创建一个路径的文件夹
+                }
+                StreamWriter sw = new StreamWriter(Path.Combine(directoryPath, filePath),true);//打开文件，并设定为追加数据
+                sw.WriteLine("开始时间"+startTime.ToString()+"——结束时间"+DateTime.Now.ToLocalTime().ToString()+"专注时间"+textBox1.Text.ToString());
+                sw.WriteLine("——————————————————————————————————————————————————————————————");
+                sw.Flush();
+                sw.Close();
+            }
+        }
+
+        /// <summary>
+        /// 控制暂停和恢复计时 按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if(button2.Text=="暂停计时")
+            {
+                timer1.Stop();
+                button2.Text = "恢复计时";
+            }
+            else
+            {
+                timer1.Start();
+                button2.Text = "暂停计时";
+            }
+        }
+        /// <summary>
+        /// 放弃本次后将一切回归初始值
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            button2.Text = "暂停计时";
+            label5.Text = "00h00m00s";
+            Timer = 0;
+            progressBar1.Value = 0;
+        }
+    }
+}
